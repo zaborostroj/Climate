966ce6f04677dae666d82c90d48fe8431f74ee7d
zaborostroj <zaborostroj@gmail.com>
2015-03-14 14:55:12 +0300

    small bug fixes

---
+++ src/SearchToolDialog.java
@@ -0,0 +1,8 @@
+/**
+  * Created by Evgeny Baskakov on 14.03.2015.
+ */
+public class SearchToolDialog {
+    public SearchToolDialog() {
+        System.out.println("search dialog constructor");
+    }
+}
